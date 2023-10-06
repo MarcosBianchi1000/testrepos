fc89935fc4d95c180e54364f9252be964307ee2a
Marcos Bianchi Guedes Alonso <marco_alonso@petrobras.com.br>
2023-10-06 16:17:18 -0300

    adicionado funcao soma

--- firstoython.py
+++ firstoython.py
@@ -1,2 +1,9 @@
 #Imprime a sa√≠da
 print ("Novo arquivo Python")
+n1 = 0
+n2 = 0
+sum = 0
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite outro número: "))
+sum = n1 + n2
+print("A soma é: ", sum)
